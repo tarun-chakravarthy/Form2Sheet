0b0ef823f6ef7f3504a6074224991a9afe88f872
Tarun Duggempudi <tarun@tonic.com.au>
2021-08-23 17:08:35 +1000

    Lenovo-services-wiring-template

---
+++ resources/views/pdf.blade.php
@@ -0,0 +1,10 @@
+//list of user data
+//TODO: add a table to pdf export
+
+
+{{-- Test --}}
+<h1>
+    This is your generated pdf
+</h1>
+
+//TODO: add a for loop that is able to take the multiple form inputs
